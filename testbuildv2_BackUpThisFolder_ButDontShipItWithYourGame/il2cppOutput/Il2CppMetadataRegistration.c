07139cc30678fc325baae53ed010b4780a2ffdfc
Golina Hulstein <golina.hulstein@student.auc.nl>
2022-09-12 21:57:11 +0200

    Bulk Commit

---
+++ testbuildv2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	4985,
+	g_Il2CppGenericTypes,
+	3062,
+	g_Il2CppGenericInstTable,
+	35199,
+	g_Il2CppGenericMethodFunctions,
+	19731,
+	g_Il2CppTypeTable,
+	44175,
+	g_Il2CppMethodSpecTable,
+	7106,
+	g_FieldOffsetTable,
+	7106,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
